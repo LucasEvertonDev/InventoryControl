3c38c91a2a2559d9354d8e508e7ae345761f958e
Lcs Everton <lcseverton@outlook.com>
2022-05-01 17:26:46 -0300

    Inclusão de Component

---
+++ InventoryControl.WebUI/ViewComponents/LayoutNaveBarViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using InventoryControl.WebUI.Identity;
+using InventoryControl.WebUI.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryControl.WebUI
+{
+    [ViewComponent(Name = "LayoutNaveBarViewComponent")]
+    public class LayoutNaveBarViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LayoutNaveBarViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var appUser = await _userManager.FindByNameAsync(
+                HttpContext.User.Identity.Name);
+
+            var vm = new LayoutNavBarViewModel
+            {
+                UserName = appUser?.UserName,
+                Email = appUser?.Email,
+            };
+
+            return View("../Shared/_LayoutNavBar.cshtml", vm);
+        }
+    }
+}
---
+++ InventoryControl.WebUI/ViewModels/LayoutNavBarViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryControl.WebUI.ViewModels
+{
+    public class LayoutNavBarViewModel : BaseViewModel, IViewModel
+    {
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
